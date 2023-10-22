4715da0a8ec776ba4ed359ae02618cb88f988bca
14337289 <guoyangru@gmail.com>
2023-10-23 02:42:18 +1100

    Finish Feature-LevelOneScreen and improve responsiveness of multiple UI

--- Assets/Scripts/AudioManager.cs
+++ Assets/Scripts/AudioManager.cs
@@ -8,23 +8,16 @@ public class AudioManager : MonoBehaviour
     [SerializeField] private AudioSource introAudio;
     [SerializeField] private AudioSource normalAudio;
 
+    void Awake() 
+    {
+        SceneManager.sceneLoaded += OnSceneLoaded;
+    }
+
     // Start is called before the first frame update
     void Start()
     {
-        //DontDestroyOnLoad(gameObject);
-        int currentSceneIndex = SceneManager.GetActiveScene().buildIndex;
-        if (currentSceneIndex == 0)
-        {
-            introAudio.loop = true;
-            introAudio.Play();
-        }
-        else
-        {
-            introAudio.loop = false;
-            introAudio.Play();
-            StartCoroutine(PlayBGAfterIntroFinish());
-        }
-
+        DontDestroyOnLoad(gameObject);
+        //int currentSceneIndex = SceneManager.GetActiveScene().buildIndex;
     }
 
     // Update is called once per frame
@@ -42,5 +35,32 @@ public class AudioManager : MonoBehaviour
 
         normalAudio.loop = true;
         normalAudio.Play();
+    }
+
+    public void OnSceneLoaded(Scene scene, LoadSceneMode mode)
+    {
+        if (scene.buildIndex == 0)
+        {
+            if (introAudio.isPlaying) 
+            {
+                introAudio.Stop();
+            }
+            if (normalAudio.isPlaying) 
+            {
+                normalAudio.Stop();
+            }
+            introAudio.loop = true;
+            introAudio.Play();
+        }
+        else if (scene.buildIndex == 1)
+        {
+            if (introAudio.isPlaying) 
+            {
+                introAudio.Stop();
+            }
+            introAudio.loop = false;
+            introAudio.Play();
+            StartCoroutine(PlayBGAfterIntroFinish());
+        }
     }
 }
--- Assets/Scripts/UIManager.cs
+++ Assets/Scripts/UIManager.cs
@@ -2,9 +2,14 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.SceneManagement;
+using UnityEngine.UI;
 public class UIManager : MonoBehaviour
 {
     // Start is called before the first frame update
+    void Awake() 
+    {
+        SceneManager.sceneLoaded += OnSceneLoaded;
+    }
     void Start()
     {
         //DontDestroyOnLoad(gameObject);
@@ -24,5 +29,26 @@ public class UIManager : MonoBehaviour
     public void LoadScendLevel() 
     {
         SceneManager.LoadScene(2);
+    }
+
+    public void LoadStartScene()
+    {
+        SceneManager.LoadScene(0);
+    }
+
+    public void OnSceneLoaded(Scene scene, LoadSceneMode mode)
+    {
+        if (scene.buildIndex == 1)
+        {
+            GameObject exitButton = GameObject.FindGameObjectWithTag("ExitButton");
+            if (exitButton != null)
+            {
+                Button quitButtonComponent = exitButton.GetComponent<Button>();
+                if (quitButtonComponent != null)
+                {
+                    quitButtonComponent.onClick.AddListener(LoadStartScene);
+                }
+            }
+        }
     }
 }
